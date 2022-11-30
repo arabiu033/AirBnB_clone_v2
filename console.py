e6fd3367f26893c7b6bb2041530f2c0e73958b88
arabiu033 <arabiu033@gmail.com>
2022-11-29 16:28:42 -0800

    7. Console 0.1

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Concole Module """
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -9,10 +11,69 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb) "
 
+    def do_create(self, line):
+        """
+         Creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id
+        """
+        if line == "BaseModel":
+            obj_instance = BaseModel()
+            onj_instance.save()
+        elif len(line) == 0:
+            print("** class name missing **")
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, line):
+        """
+        Prints the string representation of an instance based
+        on the class name and id. Ex: $ show BaseModel 1234-1234-1234
+        """
+        if len(line) == 0:
+            print("** class name missing **")
+            return
+
+        key = line.split(" ", maxsplit=1)
+        if len(key) < 2:
+            print("** instance id missing **")
+        elif key[0] != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            all_obj = strorage.all()
+            try:
+                obj = all_obj[".".join(key)]
+                print(obj)
+            except KeyError:
+                print("** no instance found **")
+
+    def do_destroy(self, line):
+        """
+        Deletes an instance based on the class name and id
+        (save the change into the JSON file). Ex: $ destroy BaseModel 1234-1234-1234
+        """
+        if len(line) == 0:
+            print("** class name missing **")
+            return
+
+        key = line.split(" ", maxsplit=1)
+        if len(key) < 2:
+            print("** instance id missing **")
+        elif key[0] != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            all_obj = strorage.all()
+            try:
+                del all_obj[".".join(key)]
+                storage.save()
+            except KeyError:
+                print("** no instance found **")
+
     def do_quit(self, line):
         """
         exit the program
         """
+        key = line.split(" ", maxsplit=1)
+        print(".".join(key))
         return True
 
     def do_EOF(self, line):
