1311e611219149d373787c224a6f560c661a0234
Aminu Rabiu <arabiu033@gmail.com>
2023-02-21 22:33:17 +0100

    9. Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,12 +4,6 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /states: HTML page with a list of all State objects.
     /states/<id>: HTML page displaying the given state with <id>.
-    
-Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
 """
 from flask import Flask, render_template
 from models import storage
@@ -20,12 +14,14 @@ app.url_map.strict_slashes = False
 
 @app.teardown_appcontext
 def closedb(exc):
-    """ to close a database session"""
+    """ to close a database session """
+    """ to close a database session """
     storage.close()
 
 
 @app.route('/cities_by_states')
 def states_list():
+    """ /states_list route """
     """ /states_list route """
     states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
