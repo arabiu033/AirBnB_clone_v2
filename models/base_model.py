ba3b2c851580888158f393eb7c12a69539246fd9
Aminu Rabiu <arabiu033@gmail.com>
2023-02-21 21:12:35 +0100

    Change datetime type to str

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,7 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    value = str(datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    value = datetime.strptime(str(value), "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
 
