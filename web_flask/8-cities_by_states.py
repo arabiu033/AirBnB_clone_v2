0b3344018f71005b7d999a64ce350b0a973db962
Aminu Rabiu <arabiu033@gmail.com>
2023-02-21 22:49:03 +0100

    Fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
-"""
+""" Starts a Flask web application. """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
