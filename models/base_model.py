1314b226b4882c19a3e8ca81777e1060ac7dca2e
Aminu Rabiu <arabiu033@gmail.com>
2023-02-21 21:30:33 +0100

    Change datetime type to str

--- models/base_model.py
+++ models/base_model.py
@@ -23,11 +23,11 @@ class BaseModel:
         Initialize instances attributes
         """
         self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.utcnow()
+        self.created_at = self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
+                    value = datetime.strptime(str(value), "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
 
