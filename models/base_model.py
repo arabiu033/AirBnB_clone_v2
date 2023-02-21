c1e47d38a0a68cf2a2c38679effdf2d49cef0bc5
Aminu Rabiu <arabiu033@gmail.com>
2023-02-21 17:16:19 +0100

    6. DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -3,30 +3,33 @@
 import models
 from datetime import datetime
 from uuid import uuid4
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
 
+Base = declarative_base()
 
 class BaseModel:
     """
     parent class to take care of the initialization,
     serialization and deserialization of your future instances
     """
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """
         Initialize instances attributes
         """
-        if len(kwargs) == 0:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-        else:
-            self.__dict__ = kwargs
-            if "__class__" in self.__dict__:
-                del self.__dict__["__class__"]
-            if not "id" in self.__dict__:
-                self.__dict__["id"] = str(uuid4())
-            self.__dict__["created_at"] = datetime.fromisoformat(str(kwargs["created_at"]))
-            self.__dict__["updated_at"] = datetime.fromisoformat(str(kwargs["updated_at"]))
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """
@@ -40,6 +43,7 @@ class BaseModel:
         updates the public instance attribute updated_at with the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -48,6 +52,7 @@ class BaseModel:
         """
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat();
-        obj_dict["updated_at"] = self.updated_at.isoformat();
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        obj_dict.pop("_sa_instance_state", None)
         return obj_dict
--- models/city.py
+++ models/city.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """ City Module """
-from .base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """
     BaseModel sub class
     """
 
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='delete')
